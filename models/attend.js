b4d5fcc94273905df59f46df64e70fd0a4adb8d1
leeseonghyun <leeseonghyun21@gmail.com>
2017-12-09 23:08:05 +0900

    minor fixed

--- models/attend.js
+++ models/attend.js
@@ -6,7 +6,6 @@ var schema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: 'User' },
   festival: { type: Schema.Types.ObjectId, ref: 'Festival' },
   content: {type: String, trim: true, required: true},
-  numLikes: {type: Number, default: 0},
   createdAt: {type: Date, default: Date.now}
 }, {
   toJSON: { virtuals: true},
